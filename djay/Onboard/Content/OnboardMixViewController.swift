605ac501df10f7cf6a8f24b429989f8ec2003f0d
Oliver Michalak <oliver@werk01.de>
2026-03-04 19:32:04 +0100

    add animation to mix screen

--- djay/Onboard/Content/OnboardMixViewController.swift
+++ djay/Onboard/Content/OnboardMixViewController.swift
@@ -14,4 +14,70 @@ class OnboardMixViewController: OnboardContentViewController {
 	@IBOutlet weak var mixLabel: UILabel!
 	@IBOutlet weak var adaView: UIImageView!
 
+	private var heroAnimator: Animator?
+	private var mixAnimator: Animator?
+	private var adaAnimator: Animator?
+
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+
+		guard heroAnimator == nil else { return }
+		heroAnimator = Animator(
+			progress: onboard.progressPublisher,
+			view: heroView,
+			steps: [
+				AnimatorStep(
+					progress: 0.5,
+					transform: CGAffineTransform(translationX: -view.bounds.width * 0.5, y: 30).scaledBy(x: 0.1, y: 0.1),
+					alpha: 0),
+				AnimatorStep(
+					progress: 1,
+					transform: .identity,
+					alpha: 1),
+				AnimatorStep(
+					progress: 1.5,
+					transform: CGAffineTransform(translationX: view.bounds.width * 0.5, y: 30).scaledBy(x: 0.1, y: 0.1),
+					alpha: 0)
+			]
+		)
+
+		mixAnimator = Animator(
+			progress: onboard.progressPublisher,
+			view: mixLabel,
+			steps: [
+				AnimatorStep(
+					progress: 0.6,
+					transform: CGAffineTransform(translationX: -view.bounds.width * 0.4, y: 30).scaledBy(x: 0.1, y: 0.1),
+					alpha: 0),
+				AnimatorStep(
+					progress: 1,
+					transform: .identity,
+					alpha: 1),
+				AnimatorStep(
+					progress: 1.4,
+					transform: CGAffineTransform(translationX: view.bounds.width * 0.4, y: 30).scaledBy(x: 0.1, y: 0.1),
+					alpha: 0)
+			]
+		)
+
+		adaAnimator = Animator(
+			progress: onboard.progressPublisher,
+			view: adaView,
+			steps: [
+				AnimatorStep(
+					progress: 0.7,
+					transform: CGAffineTransform(translationX: -view.bounds.width * 0.3, y: 30).scaledBy(x: 0.1, y: 0.1),
+					alpha: 0),
+				AnimatorStep(
+					progress: 1,
+					transform: .identity,
+					alpha: 1),
+				AnimatorStep(
+					progress: 1.3,
+					transform: CGAffineTransform(translationX: view.bounds.width * 0.3, y: 30).scaledBy(x: 0.1, y: 0.1),
+					alpha: 0)
+			]
+		)
+	}
+
 }
